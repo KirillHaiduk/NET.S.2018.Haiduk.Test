4881146df11cc569058309b530c841a3f5b83ce4
KirillHaiduk <33031000+KirillHaiduk@users.noreply.github.com>
2018-04-23 18:24:45 +0300

    Add files via upload

--- Task3.Solution/Stock.cs
+++ Task3.Solution/Stock.cs
@@ -1,31 +1,30 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Task3.Solution;
 
 namespace Task3
 {
     public class Stock
     {
-        private StockInfo stocksInfo;
+        public int USD { get; private set; }
+
+        public int Euro { get; private set; }
 
         public delegate void StockEventHandler(object sender, StockEventArgs e);
         
         public event StockEventHandler ChangeCurrency = delegate { };
 
-        public Stock()
-        {            
-            stocksInfo = new StockInfo();
+        public Stock(int usd, int euro)
+        {
+            USD = usd;
+            Euro = euro;
         }
 
         public void Market()
         {
             Random rnd = new Random();
-            stocksInfo.USD = rnd.Next(20, 40);
-            stocksInfo.Euro = rnd.Next(30, 50);
-            OnChangeCurrency(this, new StockEventArgs(stocksInfo.Euro, stocksInfo.USD));
+            this.USD = rnd.Next(20, 40);
+            this.Euro = rnd.Next(30, 50);
+            OnChangeCurrency(this, new StockEventArgs(this.Euro, this.USD));
         }
 
         protected virtual void OnChangeCurrency(object sender, StockEventArgs e) => ChangeCurrency?.Invoke(sender, e);
