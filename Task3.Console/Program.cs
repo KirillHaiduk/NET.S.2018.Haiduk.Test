6dc7120352b8d0d6a152b17c7ae5d47c3a531595
KirillHaiduk <33031000+KirillHaiduk@users.noreply.github.com>
2018-04-23 18:25:39 +0300

    Add files via upload

---
+++ Task3.Console/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Task3.Solution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stock = new Stock(10, 20);
+            var bank = new Bank("Some Bank");
+            var broker = new Broker("Some Broker");
+            bank.Register(stock);
+            broker.Register(stock);
+            stock.Market();
+            Console.ReadLine();
+        }
+    }
+}
