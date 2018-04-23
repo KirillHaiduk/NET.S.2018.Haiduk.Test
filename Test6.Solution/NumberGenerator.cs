8a30380b26febed85164f2eb2c06fa9d7936f4db
KirillHaiduk <33031000+KirillHaiduk@users.noreply.github.com>
2018-04-23 12:20:39 +0300

    Add files via upload

---
+++ Test6.Solution/NumberGenerator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test6.Solution
+{
+    public static class NumberGenerator<T>
+    {
+        public static IEnumerable<T> Generator(T x1, T x2, int number, Func<T, T, T> function)
+        {
+            if (number <= 0)
+            {
+                throw new ArgumentException(nameof(number));
+            }
+
+            if (function == null)
+            {
+                throw new ArgumentNullException(nameof(function));
+            }
+
+            return GeneratorHelper<T>(x1, x2, number, function);
+        }
+
+        private static IEnumerable<T> GeneratorHelper<T>(T x1, T x2, int number, Func<T, T, T> function)
+        {
+            T first = x1;
+            T next = x2;
+            for (int i = 0; i < number; i++)
+            {
+                T temp = next;
+                next = function(first, next);
+                first = temp;
+                yield return next;
+            }
+        }
+    }
+}
