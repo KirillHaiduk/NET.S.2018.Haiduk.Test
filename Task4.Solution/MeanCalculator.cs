07e567f30a648d9981b12194ad74975d2566bf3c
KirillHaiduk <33031000+KirillHaiduk@users.noreply.github.com>
2018-04-23 10:46:46 +0300

    Add files via upload

---
+++ Task4.Solution/MeanCalculator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4.Solution
+{
+    public class MeanCalculator : ICalculator
+    {
+        public double CalculateAverage(IEnumerable<double> values)
+        {
+            if (values == null)
+            {
+                throw new ArgumentNullException(nameof(values));
+            }
+
+            return values.Sum() / values.Count();
+        }
+    }
+}
