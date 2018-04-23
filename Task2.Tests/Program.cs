65980e6871fd45025e78f630f8539675de8916d2
KirillHaiduk <33031000+KirillHaiduk@users.noreply.github.com>
2018-04-23 21:22:07 +0300

    Add files via upload

---
+++ Task2.Tests/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Task_2.Solution;
+
+namespace Task2.Tests
+{
+    public class Program
+    {
+        static void Main()
+        {
+            var charsGenerator = new RandomCharsFileGenerator();
+            var bytesGenerator = new RandomBytesFileGenerator();
+            charsGenerator.GenerateFiles(2, 20);
+            bytesGenerator.GenerateFiles(2, 20);            
+            Console.ReadLine();
+        }
+    }
+}
