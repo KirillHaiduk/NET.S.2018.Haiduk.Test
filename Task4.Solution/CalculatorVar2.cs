32e9c5cfe8460d43414b8bda5133433b8d2ec9a4
KirillHaiduk <33031000+KirillHaiduk@users.noreply.github.com>
2018-04-23 11:01:20 +0300

    Add files via upload

---
+++ Task4.Solution/CalculatorVar2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4.Solution
+{
+    public static class CalculatorVar2
+    {
+        public static double Calculate(IEnumerable<double> values, Func<IEnumerable<double>, double> average)
+        {
+            if (values == null)
+            {
+                throw new ArgumentNullException(nameof(values));
+            }
+
+            if (average == null)
+            {
+                throw new ArgumentNullException(nameof(average));
+            }
+
+            return average(values);
+        }
+    }
+}
