d52adbdfc1c0a808a0ea1f89f094c1b93cde6381
KirillHaiduk <33031000+KirillHaiduk@users.noreply.github.com>
2018-04-23 11:58:15 +0300

    Add files via upload

---
+++ Task5.Solution/ConvertVisitor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5.Solution
+{
+    class ConvertVisitor : DocumentPartVisitor
+    {
+        private ITextConverter converter;
+
+        public ConvertVisitor(ITextConverter textConverter)
+        {
+            converter = textConverter;
+        }
+
+        public string ConvertedString { get; private set; }
+
+        protected override void Visit(BoldText boldText)
+        {
+            ConvertedString = converter.Convert(boldText);
+        }
+
+        protected override void Visit(Hyperlink hyperlink)
+        {
+            ConvertedString = converter.Convert(hyperlink);
+        }
+
+        protected override void Visit(PlainText plainText)
+        {
+            ConvertedString = converter.Convert(plainText);
+        }
+    }
+}
---
+++ Task5.Solution/DocumentPartVisitor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5.Solution
+{
+    public abstract class DocumentPartVisitor
+    {
+        public void DynamicVisit(DocumentPart documentPart) => Visit((dynamic)documentPart);
+        protected abstract void Visit(BoldText boldText);
+        protected abstract void Visit(Hyperlink hyperlink);
+        protected abstract void Visit(PlainText plainText);
+    }
+}
