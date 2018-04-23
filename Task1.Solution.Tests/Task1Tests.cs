c3d1a43cbd046b6e0d7565a25e0f7b75f245f0a7
KirillHaiduk <33031000+KirillHaiduk@users.noreply.github.com>
2018-04-23 22:41:53 +0300

    Add files via upload

--- Task1.Solution.Tests/Task1Tests.cs
+++ Task1.Solution.Tests/Task1Tests.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using NUnit.Framework;
 
 namespace Task1.Solution.Tests
@@ -11,10 +9,35 @@ namespace Task1.Solution.Tests
     public class Task1Tests
     {
         [TestCase(null, ExpectedResult = false)]
+        [TestCase("", ExpectedResult = false)]
+        [TestCase("abc123", ExpectedResult = false)]
+        [TestCase("abcdefgh123456789", ExpectedResult = false)]
+        [TestCase("1234567890", ExpectedResult = false)]
+        [TestCase("abcdefghij", ExpectedResult = false)]
+        [TestCase("abcdef123", ExpectedResult = true)]
+        [TestCase("0AaBbCc1", ExpectedResult = true)]
         public bool PasswordValidationTest(string password)
         {
-            PasswordValidation passwordValidation = new PasswordValidation(password);
-            passwordValidation.AddCheck((PasswordValidation)=>password == null);
+            var conditions = new List<Tuple<Predicate<string>, string>>();            
+            Tuple<Predicate<string>, string> t1 = Tuple.Create<Predicate<string>, string>((p) => password == null, $"{password} is null arg");
+            conditions.Add(t1);
+
+            Tuple<Predicate<string>, string> t2 = Tuple.Create<Predicate<string>, string>((p) => password == string.Empty, $"{password} is empty");
+            conditions.Add(t2);
+
+            Tuple<Predicate<string>, string> t3 = Tuple.Create<Predicate<string>, string>((p) => password.Length <= 7, $"{password} length too short");
+            conditions.Add(t3);
+
+            Tuple<Predicate<string>, string> t4 = Tuple.Create<Predicate<string>, string>((p) => password.Length >= 15, $"{password} length too long");
+            conditions.Add(t4);
+
+            Tuple<Predicate<string>, string> t5 = Tuple.Create<Predicate<string>, string>((p) => !password.Any(char.IsLetter), $"{password} hasn't alphanumerical chars");
+            conditions.Add(t5);
+
+            Tuple<Predicate<string>, string> t6 = Tuple.Create<Predicate<string>, string>((p) => !password.Any(char.IsNumber), $"{password} hasn't digits");
+            conditions.Add(t6);
+
+            var passwordValidation = new PasswordValidation(password, conditions);
             return passwordValidation.Validate(password);
         }
     }
